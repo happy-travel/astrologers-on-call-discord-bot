03aa66ffdf2d798a248c26a9a0a397e4c3074ff2
Kirill Taran <kirill.taran@hotmail.com>
2020-12-09 00:48:14 +0300

    Enlist method refactored

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const config = require('./config.json');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Config = require('./config.json');
+const { Discord, Client, Intents } = require('discord.js');
+
+const intents = new Intents([
+    Intents.NON_PRIVILEGED, 
+	"GUILD_MEMBERS", 
+	"GUILD_PRESENCES"
+]);
+
+const client = new Client({ ws: { intents } });
 
 
 function isMatch(command, content) {
-	return config.prefixes.some(prefix => content === `${prefix}${command}`)
+	return Config.prefixes.some(prefix => content === `${prefix}${command}`);
+};
+
+
+function isStart(command, content) {
+	return Config.prefixes.some(prefix => content.startsWith(`${prefix}${command}`));
 };
 
 
@@ -16,29 +28,83 @@ function getElapsedWeekCount() {
 };
 
 
-client.on('message', message => {
-	if (isMatch('proclaim', message.content)) {
-		let engineers = config.engineers;
-		let elapsedWeeks = getElapsedWeekCount();
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+client.on('message', async (message) => {
+	try {
+		if (isMatch('proclaim', message.content)) {
+			let engineers = Config.engineers;
+			let elapsedWeeks = getElapsedWeekCount();
+			
+			let position = elapsedWeeks % engineers.length;
+			let engineer = engineers[position];
+	
+			let proclamation = `🔮 Астрологи объявили эту неделю неделей ${engineer.gen}. ${engineer.nom} удваивает количество закрытых багов 🔮`;
+	
+			message.channel.send(proclamation);
+		}
+	
+		if (isStart('scripter', message.content) || isStart('enlist', message.content)) {
+			let participants = [];
 		
-		let position = elapsedWeeks % engineers.length;
-		let engineer = engineers[position];
+			if (0 < message.mentions.users.size) {
+				for (let [key, user] of message.mentions.users) {
+					if (user.bot === true) {
+						continue;
+					}
+	
+					participants.push(key);
+				}
+			}
 
-		let proclamation = `🔮 Астрологи объявили эту неделю неделей ${engineer.gen}. ${engineer.nom} удваивает количество закрытых багов 🔮`;
+			let members = ( await (await client.guilds.fetch(Config.guildId)).members.fetch());
+		
+			let onlineMembers = [];
+			for(let [key, member] of members) {
+				if (member.deleted === true) {
+					continue;
+				}
 
-		message.channel.send(proclamation);
-	}
+				if (member.user.bot == undefined || member.user.bot === true) {
+					continue;
+				}
 
-	if (isMatch('scripter', message.content)) {
-		let scripters = config.scripters;
-		let elapsedWeeks = getElapsedWeekCount();
+				if (member.presence.status !== 'online') {
+					continue;
+				}
 
-		let position = elapsedWeeks % scripters.length;
-		let scripter = scripters[position];
+				onlineMembers.push(member);
+			};
 
-		let msg = `📜 ${scripter}`;
+			if (0 < message.mentions.roles.size) {
+				for (let [key, role] of message.mentions.roles) {
+					if (role.deleted === true) {
+						continue;
+					}
+	
+					let roleMembers = onlineMembers.filter(member => member._roles.includes(key));
+					participants.push(roleMembers.map(member => member.user.id));
+				}
+			}
 
-		message.channel.send(msg);
+			if (participants.length === 0 ) {
+				participants = onlineMembers.map(member => member.user.id);;
+			}
+
+			if (participants.length !== 0) {
+				let position = getRandomInt(participants.length);
+				message.channel.send(`📜 Астрологи выбрали <@${participants[position]}>`);
+
+				return;
+			}
+
+			message.channel.send('🔮 Астрологи объявили, что выбирать не из кого 🔮');
+		}
+	} catch (e) {
+		message.channel.send(`🔮 Астрологи многозначительно объявили следующее: «${e.message}» 🔮`);
 	}
 });
 
